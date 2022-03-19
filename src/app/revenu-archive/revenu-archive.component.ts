de238c761edfee819c6ae2e029f92616daebdf9f
Bri11ant <jbrillantf@gmail.com>
2022-03-19 18:03:06 +0300

    menu mis en place

---
+++ src/app/revenu-archive/revenu-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-revenu-archive',
+  templateUrl: './revenu-archive.component.html',
+  styleUrls: ['./revenu-archive.component.scss']
+})
+export class RevenuArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
