57b427355a32b42a7063f0ccb988771773227a07
ferdinandjuko <ferdinandjuko@gmail.com>
2022-03-19 20:22:45 +0300

    Objet:revenu

--- library_manager/juko/object/revenu.ts
+++ library_manager/juko/object/revenu.ts
@@ -1,5 +1,6 @@
 export class Revenu {
-    constructor(public date: string,
+    constructor(public id_revenu: number,
+        public date: string,
         public type: string,
         public source: number,
         public montant: number){
@@ -8,7 +9,8 @@ export class Revenu {
 }
 
 export class revenuJournalier{
-    constructor(public date: string,
+    constructor(public id_revenuJournalier: number,
+        public date: string,
         public montant : number) {
         
     }
