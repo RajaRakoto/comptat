706cafa30da898b57ca68dfa89dbe887860a7360
ferdinandjuko <ferdinandjuko@gmail.com>
2022-03-20 00:21:32 +0300

    totalMontant

--- library_manager/juko/functions/totalMontant.js
+++ library_manager/juko/functions/totalMontant.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 exports.Depense = exports.Revenu = void 0;
+
 var Revenu = /** @class */ (function () {
     function Revenu(id_revenu, date, libelle, source, montant) {
         this.id_revenu = id_revenu;
@@ -11,6 +12,7 @@ var Revenu = /** @class */ (function () {
     }
     return Revenu;
 }());
+
 exports.Revenu = Revenu;
 function totalReven(revenuTotal) {
     var totalRevenu;
@@ -20,6 +22,7 @@ function totalReven(revenuTotal) {
     }
     return totalRevenu;
 }
+
 var Depense = /** @class */ (function () {
     function Depense(id_depense, date, libelle, motif, montant) {
         this.id_depense = id_depense;
@@ -30,6 +33,7 @@ var Depense = /** @class */ (function () {
     }
     return Depense;
 }());
+
 exports.Depense = Depense;
 function totalDepense(depenseTotal) {
     var totalDepense;
