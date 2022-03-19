4d2d2aec59ad8cf5b821ceded300947750a1051a
StanyNilaina <stanyrazafindrakototo@gmail.com>
2022-03-19 22:59:45 +0300

    creation de revenu.service

--- src/app/revenu/revenu.component.ts
+++ src/app/revenu/revenu.component.ts
@@ -1,28 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-export interface Revenu {
-  type: string;
-  heure: string;
-  source: string;
-  montant: number;
-}
-
-const REVENU_DATA: Revenu[] = [
-  {
-    heure: '09:47',
-    type: 'Massage',
-    source: 'IIF3MG Analakely',
-    montant: 30000,
-  },
-  { heure: '13:09', type: 'Formation', source: 'Formation', montant: 220000 },
-  {
-    heure: '15:31',
-    type: 'Aide à domicile',
-    source: 'M4C11 Anosy',
-    montant: 50000,
-  },
-  { heure: '17:11', type: 'Garde malade', source: 'HJRA', montant: 9000 },
-];
+import { REVENU_DATA } from './revenu.service';
 
 @Component({
   selector: 'app-revenu',
@@ -36,5 +13,7 @@ export class RevenuComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.revenus);
+  }
 }
