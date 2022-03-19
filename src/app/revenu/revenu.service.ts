bb48a4e366bbdef2714b7c1567e6d26a7e5b6702
StanyNilaina <stanyrazafindrakototo@gmail.com>
2022-03-19 22:59:45 +0300

    creation de revenu.service

---
+++ src/app/revenu/revenu.service.ts
@@ -0,0 +1,14 @@
+export class Revenu {
+    constructor(
+        public id_revenu: number,
+        public heure: string,
+        public type: string,
+        public source: string,
+        public montant: number){
+
+    }
+}
+export const revenu01= new Revenu(1,'09:47','Massage','HJRA',30000);
+export const revenu02= new Revenu(2,'15:31', 'Formation','Formation',220000);
+export const revenu03= new Revenu(3,'16:11', 'Vente','Client',210000);
+export const REVENU_DATA =[revenu01,revenu02,revenu03];
